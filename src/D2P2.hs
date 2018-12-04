76474df18e8685acac0dc94edb6a6170d4eb3ef6
Bjørn Arild Kristiansen <bjornakr@gmail.com>
2018-12-04 23:47:08 +0100

    Fix stuff

--- src/D2P2.hs
+++ src/D2P2.hs
@@ -8,16 +8,16 @@ module D2P2 where
       then isMatch as bs strike
       else isMatch as bs (strike + 1)
 
-  matchFirstWithRest (_:[]) = Nothing
-  matchFirstWithRest (id0:id1:ids) =
+  matchHeadWithRest (_:[]) = Nothing
+  matchHeadWithRest (id0:id1:rest) =
     if isMatch id0 id1 0  -- take 0 away
-      then Just (id0, id1)  --fix
-      else matchFirstWithRest (id0:ids)
+      then Just (id0, id1)
+      else matchHeadWithRest (id0:rest)
 
   checkAllIds [] = "No matching ids :'("
   checkAllIds ids@(_:rest) = 
-    case matchFirstWithRest ids of
+    case matchHeadWithRest ids of
       Nothing -> checkAllIds rest
       Just (id0, id1) -> intersect id0 id1
 
-  main = interact (show . checkAllIds . words)
+  main = interact (checkAllIds . words)
